63fbaf0c7331ead9b9efd1bf1d39a135f8c9968f
Marvin Korir <marvinkorir.mk@gmail.com>
2023-03-02 11:00:03 +0000

    instrument x-ray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,9 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+#Xray -------------------------------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 
 #Honeycomb app.py updates
@@ -31,6 +34,11 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+# X-RAY -----------------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 app = Flask(__name__)
 
