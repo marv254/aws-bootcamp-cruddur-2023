da3f6427d1470e44c3b400f09b2bc8d2ea5d8e8c
Marvin Korir <marvinkorir.mk@gmail.com>
2023-03-20 12:36:34 +0000

    updated create activities

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,4 +1,3 @@
-import uuid
 from datetime import datetime, timedelta, timezone
 
 from lib.db import db
@@ -45,6 +44,7 @@ class CreateActivity:
     else:
       expires_at = (now + ttl_offset)
       uuid = CreateActivity.create_activity(user_handle,message,expires_at)
+
       object_json = CreateActivity.query_object_activity(uuid)
       model['data'] = object_json
     return model
