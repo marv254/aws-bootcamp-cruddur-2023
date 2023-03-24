7e1409ed17f0ca3a3b0eda569e71ff2daf999b4e
Marvin Korir <marvinkorir.mk@gmail.com>
2023-03-24 16:20:15 +0000

    fixing message not populating on cruddur frontend

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,10 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
-  ('Londo Mollari', 'lmollari@centari.com','londo','MOCK');
+  ('Andrew Brown','devopsmarv@gmail.com' , 'andrewbrown' ,'1a0ba4cc-b6a6-4da4-80ba-499909c02061'),
+  ('Marvin Korir','marvinkorir.mk@gmail.com' , 'marvinkorir' ,'c6ced85c-4982-4d7e-ba6c-9189ac670b0d'),
+  ('Iam Shmoney','iamshmoney@gmail.com' , 'iamshmoney' ,'4ab50a47-b433-4bad-9d81-eeeb4b7383b7')
+  ;
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
