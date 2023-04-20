5b54548c1d468133648cc997858d40386426dd50
Marvin Korir <marvinkorir.mk@gmail.com>
2023-04-20 14:10:55 +0000

    implement user edit page

--- aws/lambdas/process-images/index.js
+++ aws/lambdas/process-images/index.js
@@ -1,5 +1,5 @@
 const process = require('process');
-const {getClient, getOriginalImage, processImage, uploadProcessedImage} = require('./s3-image-processing.js');
+const {getClient, getOriginalImage, processImage, uploadProcessedImage} = require('./s3-image-processing.js')
 const path = require('path');
 
 const bucketName = process.env.DEST_BUCKET_NAME
@@ -11,16 +11,14 @@ const height = parseInt(process.env.PROCESS_HEIGHT)
 client = getClient();
 
 exports.handler = async (event) => {
-  console.log('event',event)
-
   const srcBucket = event.Records[0].s3.bucket.name;
   const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
   console.log('srcBucket',srcBucket)
   console.log('srcKey',srcKey)
 
   const dstBucket = bucketName;
-  filename = path.parse(srcKey).name
 
+  filename = path.parse(srcKey).name
   const dstKey = `${folderOutput}/${filename}.jpg`
   console.log('dstBucket',dstBucket)
   console.log('dstKey',dstKey)
