55c139e669f09dba792b4838f31395e492518b02
Marvin Korir <marvinkorir.mk@gmail.com>
2023-04-20 17:44:27 +0000

    make sure that we can upload an avatar

--- frontend-react-js/src/lib/CheckAuth.js
+++ frontend-react-js/src/lib/CheckAuth.js
@@ -25,7 +25,6 @@ export async function checkAuth(setUser){
     })
     return Auth.currentSession()
   }).then((cognito_user_session) => {
-      console.log('cognito_user_session',cognito_user_session);
       localStorage.setItem("access_token", cognito_user_session.accessToken.jwtToken)
   })
   .catch((err) => console.log(err));
